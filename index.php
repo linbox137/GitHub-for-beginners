8f4ee8b71e937adebc331694da61874914c74b7d
linbox <46384247+linbox137@users.noreply.github.com>
2019-01-19 07:22:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index</title>
+<meta name="Description" content ="Kinomonster - это портал о кино"/>
+<meta name="Keywords" content="Фильмы, фильмы он-лайн, hd"/>
+<link rel ="stylesheet" href ="asset/CSS/style.css"/>
+</head>
+
+<body>
+<div class="main">
+  <div class="header">
+    <div class="logo">
+      <div class="logotext">
+        <h1><a href="\">КиноМонстр</a> </h1>
+        <h2 class="text_logo">Кино Это страсть миллионов</h2>
+      </div>
+      <div class="menubar">
+        <ul class="menu">
+          <li>batton1</li>
+          <li>batton2</li>
+          <li>batton3</li>
+          <li>batton4</li>
+          <li>batton5</li>
+          <li>batton6</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
